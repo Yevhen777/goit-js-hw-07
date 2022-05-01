ce35d1dbd52ec723c999f9c7708d65774c97d642
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-05-01 22:17:01 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const allGalleryImage = galleryItems.map(
+  ({ preview, original, description }) => {
+    gallery.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${preview}">
+    <img class="gallery__image" src="${original}" alt="${description}" />
+        </a>
+    </div>`
+    );
+  }
+);
+
+const galleryImageFunction = (e) => {
+  const instance = basicLightbox.create(`
+    <img src="e.target.dataset.src" width="800" height="600">
+`);
+
+  instance.show();
+  console.log(instance);
+};
+
+gallery.addEventListener("click", galleryImageFunction);
